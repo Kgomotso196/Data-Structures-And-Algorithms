4400f98400824c1e39fe606f54b510cd39954adf
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-12-02 19:53:43 +0200

    Add

---
+++ Challenges/Challenge16/challenge.js
@@ -0,0 +1,16 @@
+// Start with your code from last challenge.
+
+const list = new LinkedList()
+
+list.add(3)
+list.add(5)
+list.addAt(1, 11)
+list.addAt(0, 13)
+
+console.log(list.get(2))
+// => 11
+
+console.log(list.get(3))
+// => 5
+
+module.exports = LinkedList
